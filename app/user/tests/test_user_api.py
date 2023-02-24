3277c42fa08a4310104c69c4946fa14833114f86
ryanbakerucalgary <ryan.baker1@ucalgary.ca>
2023-02-23 20:41:05 -0700

    fixed test_user_api for missing emails

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -94,6 +94,14 @@ class PublicUserApiTests(TestCase):
             self.assertNotIn('token', res.data)
             self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+            
     def test_create_token_blank_password(self):
         """Test posting a blank password returns an error."""
         payload = {'email': 'test@example.com', 'password': ''}
